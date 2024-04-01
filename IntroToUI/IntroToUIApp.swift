7dbafd8074992776240ff7313414cc62aa12597a
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-03-31 21:05:56 -0500

    Initial Commit

---
+++ IntroToUI/IntroToUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  IntroToUIApp.swift
+//  IntroToUI
+//
+//  Created by Tabita Sadiq on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct IntroToUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
